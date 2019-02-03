fb3512ad51e5d01bed7e88b311c6a6e65d575628
Yuki Koyama <yuki@koyama.xyz>
2019-02-03 09:30:53 +0900

    Add test code for parallel_map (#4)

--- tests/test-parallel-for.cpp
+++ tests/test-parallel-for.cpp
@@ -2,22 +2,42 @@
 #include <parallel-util.hpp>
 
 using parallelutil::parallel_for;
+using parallelutil::parallel_map;
 
 int main()
 {
     // Array of random numbers
-    std::vector<int> numbers = { 4, 2, 90, 58, 19, 59, 18, 24, 9 };
-    
-    // Process of squaring the i-th number
-    auto square = [&numbers](int i)
+    const std::vector<int> numbers = { 4, 2, 90, 58, 19, 59, 18, 24, 9 };
+
+    // -------------------------------------------------------------------------
+    // parallel_for
+    // -------------------------------------------------------------------------
+
+    std::vector<int> parallel_for_results(numbers.size());
+
+    // Process of squaring the i-th number in an input array and store it
+    auto square_i_th_element = [&numbers, &parallel_for_results](int i)
     {
-        numbers[i] = numbers[i] * numbers[i];
+        parallel_for_results[i] = numbers[i] * numbers[i];
     };
-    
+
     // Perform the process in parallel
     // The result is equivalent to:
     // for (int i = 0; i < numbers.size(); ++ i) { square(i); }
-    parallel_for(numbers.size(), square);
-    
+    parallel_for(numbers.size(), square_i_th_element);
+
+    // -------------------------------------------------------------------------
+    // parallel_map
+    // -------------------------------------------------------------------------
+
+    // Process of squaring a number
+    auto square = [](int number)
+    {
+        return number * number;
+    };
+
+    // Perform "map" operation in parallel
+    const std::vector<int> parallel_map_results = parallel_map(numbers, square);
+
     return 0;
 }
